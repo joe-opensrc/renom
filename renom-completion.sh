3099d41d21e4264c9253b2c6847ba006a3892e68
Joe OpenSrc <joe-opensrc@protonmail.ch>
2025-11-03 13:36:49 +0100

    add first attempt
    file handling still undecided, so this will do for now ;)

---
+++ renom-completion.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+_renom(){
+  local regmodsDir=~/.config/renom/regmods
+  local cur prev words cword;
+  _init_completion || return;
+
+  case "${prev}" in
+    "renom") COMPREPLY=("-R");;
+    "-R") COMPREPLY=( 
+            $( compgen -W "$( find . ${regmodsDir} -mindepth 1 -type f -name '*.rxs' -printf '%p\n' )" -- ${cur} ) );;
+    *) _comp_compgen_filedir; return;;
+  esac
+}
+
+complete -F _renom renom
